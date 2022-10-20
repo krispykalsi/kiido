f29a6ced276d9f507a3ebc7d858f10e29561ba6e
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2022-10-21 02:52:18 +0530

    - add setup instructions in readme - add cli flag to run dummy repository

--- app/README.md
+++ app/README.md
@@ -1,2 +1,17 @@
-## Frontend
+## ⚙️ Setup
 
+1. Make sure you have [Flutter](https://docs.flutter.dev/get-started/install) installed.
+2. Start an emulator or connect a physical device.
+3. Get the dependencies
+   ```shell
+   flutter pub get
+   ```
+4. Run the app.
+   ```shell
+   flutter run --release
+   ```
+   > **Warning** You need to setup the backend server first!<br>
+   > If you want to run it offline, run the following command instead; it runs the app with some hardcoded dummy data
+   ```shell
+   flutter run --release --dart-define="dummy="
+   ```
--- app/lib/data/repository.dart
+++ app/lib/data/repository.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:kiido/data/dummy_repository.dart';
 
 import 'azure_repository.dart';
 import 'model/category.dart';
@@ -13,8 +14,13 @@ abstract class Repository {
   Future<List<Thing>> fetchThingsOfCategory(BigInt categoryId);
 
   static final provider = Provider((ref) {
-    final client = http.Client();
-    ref.onDispose(client.close);
-    return AzureRepository(client);
+    const shouldRunDummyServer = bool.hasEnvironment("dummy");
+    if (shouldRunDummyServer) {
+      return DummyRepository();
+    } else {
+      final client = http.Client();
+      ref.onDispose(client.close);
+      return AzureRepository(client);
+    }
   });
 }
